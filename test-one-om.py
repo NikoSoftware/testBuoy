47d9dd5ddb69e3c76952cfa8e6e1816d235df519
niko <niko@nikodeMacBook-Pro.local>
2025-06-25 18:31:09 +0800

    测试om

--- test-one-om.py
+++ test-one-om.py
@@ -9,30 +9,24 @@ def infer_api_static():
     image_path = 'datasets/cat_dog_split/images/train/cat_72.jpg'
     image = cv2.imread(image_path)
 
-    # create session of om model for inference
     session = InferSession(device_id, model_path)
-
-    # create new numpy data according inputs info
     shape0 = session.get_inputs()[0].shape
-    print(shape0)
     height, width = shape0[2], shape0[3]
     resized_image = cv2.resize(image, (width, height))
     image_array = np.array(resized_image).astype(np.float32)
-
     feeds = [image_array]
 
-    # execute inference, inputs is ndarray list and outputs is ndarray list
     outputs = session.infer(feeds, mode='static')
-    print(outputs[0].shape)
 
-    np.set_printoptions(threshold=np.inf)
-    print(outputs[0])
+    # 处理输出数据
+    output = outputs[0].squeeze(0)  # 去除 batch 维度
+    output = output.transpose(1, 2, 0)  # CHW -> HWC
+    output = output.astype(np.uint8)  # 转换为 uint8
 
-    cv2.imwrite('./img/output.png', outputs[0])
+    cv2.imwrite('./img/output.png', output)
 
-
-    # free model resource and device context of session
-    session.free_resource()
+    # 正确释放资源
+    session.destroy()  # 使用 destroy() 而非 free_resource()
 
 
 infer_api_static()
